2bb4c0e4a77702b60230402f0bbb45d502a6b384
adriancuadrado <adriancuadradochavarria97@gmail.com>
2022-04-17 17:32:15 +0200

    Improved error messages

--- extension.js
+++ extension.js
@@ -1,23 +1,35 @@
 const vscode = require('vscode');
 const execSync = require('child_process').execSync;
 
-let npmInstallReminderActivated = true;
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-async function activate(context) {
+async function activate() {
 
   const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
   let checkPackageLock = true;
   let commitId;
 
+  const throwsException = f => { try { f(); } catch(e) { return true; } return false; };
+
+  const getCommandsNotPresent = programs => programs.map(program =>  [program, throwsException(() => execSync(`${program} --version`))]);
+
+  const unavailableCommands = getCommandsNotPresent(['git', 'npm']).filter(([,isNotAvailable]) => isNotAvailable);
+  if(unavailableCommands.length) {
+    unavailableCommands.forEach(([program]) => {
+      vscode.window.showErrorMessage(`Couldn't execute ${program}. Please check it can be found in the PATH environment variable and has the correct permissions. Then restart VSCode.`);
+    });
+    return;
+  }
+
   const execInWorkspace = command => execSync(command, { cwd: vscode.workspace.workspaceFolders[0].uri.fsPath }).toString();
 
-  try {
-    while(npmInstallReminderActivated) {
+  if(throwsException(() => execInWorkspace('git status'))) {
+    vscode.window.showErrorMessage('No git repository detected. Please run "git init" or "git clone <repo>". Then restart VSCode.');
+    return;
+  }
+
+  while(true) {
+    try {
       if(checkPackageLock) {
-        let output = execInWorkspace('git diff --name-only @ @{1}');
+        let output = execInWorkspace('git diff --name-only HEAD HEAD@{1}');
         if(output.split('\n').find(f => f.endsWith('package-lock.json'))) {
           checkPackageLock = false;
           commitId = execInWorkspace('git rev-parse HEAD').toString();
@@ -45,18 +57,10 @@ async function activate(context) {
         }
       }
       await sleep(1_000);
+    } catch(e) {
+      console.error(e);
     }
-  } catch(e) {
-    vscode.window.showErrorMessage("An error has occurred. Please make sure you have git and npm installed, and that the project has git initialized and at least 2 commits. Then please restart this plugin by deactivating and reactivating it.");
-    console.error(e);
   }
 }
 
-function deactivate() {
-  npmInstallReminderActivated = false;
-}
-
-module.exports = {
-  activate,
-  deactivate
-}
+module.exports = { activate };
